57157e078f96021b6a68003a5d02184224d15ea3
wjdgh4325 <134389765+wjdgh4325@users.noreply.github.com>
2023-09-02 11:42:43 +0900

    Update train_iteration_scal.py

--- train_iteration_scal.py
+++ train_iteration_scal.py
@@ -1,7 +1,7 @@
 import os
 
 for i in range(1, 51):
-    for j in [4, 7, 20, 30, 40, 50, 60, 70, 80, 90]:
+    for j in [1, 10, 100, 1000]:
         for k in [1, 2, 5, 10, 20, 50]:
             print("seed:", i, "lambda:", j, "s:", k)
             file_list = "train.py --name scal --dataset synthetic --data_dir data/lognormal_54.89%/ --batch_size 200 --censor True --lam " + str(j) + " --model MTLRNN --model_dist mtlr --num_s " + str(k) + " --num_epochs 2000 --lr 1e-3 --optimizer adam --seed " + str(i)
